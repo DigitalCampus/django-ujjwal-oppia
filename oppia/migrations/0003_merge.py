c88eb1d396c0b653cc85d294002b345415891a61
sripooja <sripooja@sripooja-desktop>
2015-10-27 14:07:06 +0530

    merge file

---
+++ oppia/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oppia', '0002_auto_20151013_1413'),
+        ('oppia', '0002_auto_20151013_1422'),
+    ]
+
+    operations = [
+    ]
